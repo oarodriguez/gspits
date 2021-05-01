f2fd18c75c02e078b12fdd9e2d48882571b7c4cd
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2021-05-01 16:23:25 -0500

    Use sphinx-doc for generating the project documentation

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. gspits documentation master file, created by
+   sphinx-quickstart on Sat May  1 15:52:12 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to gspits's documentation!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- tasks.py
+++ tasks.py
@@ -4,7 +4,9 @@ Collection of development tasks.
 Usage:
     python -m tasks TASK-NAME
 """
-
+import shutil
+from enum import Enum, unique
+from pathlib import Path
 from subprocess import run
 from typing import List
 
@@ -12,13 +14,60 @@ import typer
 
 from gspits import __version__
 
+PROJECT_DIR = Path(__file__).parent
+SRC_DIR = PROJECT_DIR / "src"
+TESTS_DIR = PROJECT_DIR / "tests"
+DOCS_DIR = PROJECT_DIR / "docs"
+DOCS_SOURCE_DIR = DOCS_DIR / "source"
+DOCS_BUILD_DIR = DOCS_DIR / "build"
+TASKS_FILE = PROJECT_DIR / "tasks.py"
+
+ISORT_CMD = "isort"
+BLACK_CMD = "black"
+PYDOCSTYLE_CMD = "pydocstyle"
+FLAKE8_CMD = "flake8"
+MYPY_CMD = "mypy"
+PYTEST_CMD = "pytest"
+SPHINX_BUILD_CMD = "sphinx-build"
+
 # Arguments to pass to subprocess.run for each task.
-FORMAT_ARGS = ["black", "tasks.py", "src", "tests"]
-ISORT_ARGS = ["isort", "tasks.py", "src", "tests"]
-PYDOCSTYLE_ARGS = ["pydocstyle", "tasks.py", "src", "tests"]
-FLAKE8_ARGS = ["flake8", "tasks.py", "src", "tests", "--statistics"]
-PYTEST_ARGS = ["pytest"]
-MYPY_ARGS = ["mypy", "src", "tests"]
+BUILD_DOCS_ARGS = [
+    SPHINX_BUILD_CMD,
+    str(DOCS_SOURCE_DIR),
+    str(DOCS_BUILD_DIR),
+]
+FORMAT_ARGS = [
+    BLACK_CMD,
+    str(TASKS_FILE),
+    str(SRC_DIR),
+    str(TESTS_DIR),
+    str(DOCS_DIR),
+]
+ISORT_ARGS = [
+    ISORT_CMD,
+    str(TASKS_FILE),
+    str(SRC_DIR),
+    str(TESTS_DIR),
+]
+PYDOCSTYLE_ARGS = [
+    PYDOCSTYLE_CMD,
+    str(TASKS_FILE),
+    str(SRC_DIR),
+    str(TESTS_DIR),
+]
+FLAKE8_ARGS = [
+    FLAKE8_CMD,
+    str(TASKS_FILE),
+    str(SRC_DIR),
+    str(TESTS_DIR),
+    "--statistics",
+]
+PYTEST_ARGS = [PYTEST_CMD]
+MYPY_ARGS = [
+    MYPY_CMD,
+    str(TASKS_FILE),
+    str(SRC_DIR),
+]
 
 
 def _run(command: List[str]):
@@ -86,6 +135,42 @@ def format_check():
     _run(FORMAT_ARGS)
     _run(ISORT_ARGS)
     _run(MYPY_ARGS)
+
+
+@unique
+class DocFormat(str, Enum):
+    """Document Formats"""
+
+    HTML = "html"
+
+
+doc_format_spec = typer.Option(
+    default="html", help="Generated documentation format"
+)
+
+
+@app.command()
+def build_docs(doc_format: DocFormat = doc_format_spec):
+    """Build the documentation."""
+    BUILD_DOCS_ARGS.extend(["-b", doc_format])
+    _run(BUILD_DOCS_ARGS)
+
+
+@unique
+class CleaningTask(str, Enum):
+    """Cleaning tasks."""
+
+    DOCS = "docs"
+
+
+what_spec = typer.Argument(default=None, help="Cleaning task to perform")
+
+
+@app.command()
+def clean(task: CleaningTask = what_spec):
+    """Build the documentation."""
+    if task is None or task is CleaningTask.DOCS:
+        shutil.rmtree(DOCS_BUILD_DIR, ignore_errors=True)
 
 
 if __name__ == "__main__":
