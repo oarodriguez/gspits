385b749451b6d9c3c5c34b3690de938327ccacc7
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2021-06-30 14:44:56 -0500

    Add an implementation of the BEPS method to find 1D ground states

---
+++ src/gspits/one_dim/gs_single.py
@@ -0,0 +1,211 @@
+"""Routines for getting the ground-state and related properties."""
+
+import logging
+from collections import deque
+from math import pi
+from typing import Iterable
+
+import numpy as np
+from attr import dataclass
+from scipy import fft
+
+from gspits import Mesh, TimeMesh
+from gspits.one_dim import Hamiltonian, State
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True, slots=True)
+class BEPSSolverState:
+    """BEPSSolver state at each time step."""
+
+    __slots__ = ()
+
+    # The spatial mesh.
+    mesh: Mesh
+
+    # The final state.
+    wave_func: np.ndarray
+
+    # Wave vectors of the wave function Fourier expansion.
+    wave_vectors: np.ndarray
+
+    # FFT of the wave function.
+    wave_func_fft: np.ndarray
+
+    # Energy.
+    energy: float
+
+    # Chemical potential
+    chemical_potential: float
+
+    @property
+    def state(self):
+        """Quantum state associated with the solver state."""
+        return State(self.mesh, self.wave_func)
+
+
+@dataclass(frozen=True, slots=True)
+class BEPSSolver(Iterable[BEPSSolverState]):
+    """Backward Euler Pseudo-Spectral solver."""
+
+    __slots__ = ()
+
+    # The system Hamiltonian.
+    hamiltonian: Hamiltonian
+
+    # The spatial mesh.
+    mesh: Mesh
+
+    # Temporal mesh used for the imaginary-time evolution.
+    time_mesh: TimeMesh
+
+    # State used for starting the procedure.
+    ini_state: State
+
+    # Absolute tolerance for the convergence test at each time step.
+    abs_tol: float = 1e-4
+
+    # Absolute tolerance for the convergence test used at the inner
+    # loop executed at each time step.
+    time_step_abs_tol: float = 1e-8
+
+    # Maximum number of steps for the inner loop executed at each time step.
+    max_time_step_iters: int = 128
+
+    @property
+    def ground_state(self):
+        """Get the ground state yield by this solver."""
+        states_dq = deque(self, maxlen=1)
+        last_solver_state: BEPSSolverState = states_dq.pop()
+        return State(
+            mesh=last_solver_state.mesh, wave_func=last_solver_state.wave_func
+        )
+
+    @staticmethod
+    def wave_vectors(mesh: Mesh) -> np.ndarray:
+        """Get the wave vectors of the wave function Fourier expansion."""
+        return 2 * pi * fft.fftfreq(mesh.num_segments, mesh.step_size)
+
+    def __iter__(self):
+        """Make this class instances iterable objects."""
+        mesh = self.mesh
+        int_factor = self.hamiltonian.int_factor
+        domain_mesh = mesh.array
+        domain_extent = mesh.upper_bound - mesh.lower_bound
+        step_size = mesh.step_size
+        wave_vectors = self.wave_vectors(mesh)
+        time_step = self.time_mesh.time_step
+        num_time_steps = self.time_mesh.num_steps
+        max_time_step_iters = self.max_time_step_iters
+        abs_tol = self.abs_tol
+        time_step_abs_tol = self.time_step_abs_tol
+        ext_potential = self.hamiltonian.external_potential
+        ext_pot_array = ext_potential(domain_mesh)
+        # Imaginary time evolution.
+        wave_func_tdx = self.ini_state.wave_func
+        wave_func_tdx_diff = np.nan
+        break_evolve = False
+        # Logging information.
+        logger.debug("Starting BEPS")
+        for step_tdx in range(num_time_steps + 1):
+            wave_func_tdx_abs_sqr = np.abs(wave_func_tdx) ** 2
+            wave_func_tdx_abs_quartic = wave_func_tdx_abs_sqr ** 2
+            full_pot_array = ext_pot_array + int_factor * wave_func_tdx_abs_sqr
+            alpha = 0.5 * (full_pot_array.min() + full_pot_array.max())
+            int_energy_array = (
+                ext_pot_array * wave_func_tdx_abs_sqr
+                + 0.5 * int_factor * wave_func_tdx_abs_quartic
+            )
+            wave_func_tdx_fft = fft.fft(wave_func_tdx)
+            energy = (domain_extent / 4) * np.sum(
+                wave_vectors ** 2 * np.abs(wave_func_tdx_fft) ** 2
+            ) + step_size * np.sum(int_energy_array)
+            chemical_pot = energy + 0.5 * int_factor * step_size * np.sum(
+                wave_func_tdx_abs_quartic
+            )
+            yield BEPSSolverState(
+                mesh=self.mesh,
+                wave_func=wave_func_tdx,
+                wave_func_fft=wave_func_tdx_fft,
+                wave_vectors=wave_vectors,
+                energy=energy,
+                chemical_potential=chemical_pot,
+            )
+            # If we have reached the maximum number of steps, then do not
+            # make any other calculations. Just try to go to the next loop
+            # step and exhaust it.
+            if step_tdx == num_time_steps:
+                continue
+            if break_evolve:
+                break
+            # Start iterative procedure to find the wave function
+            # after a single time step.
+            wave_func_idx = wave_func_tdx
+            wave_func_idx_diff = np.nan
+            for idx in range(max_time_step_iters + 1):
+                stabilized_term = (alpha - full_pot_array) * wave_func_idx
+                next_wave_func_idx_fft = (
+                    2
+                    * (
+                        wave_func_tdx_fft
+                        + time_step * fft.fft(stabilized_term)
+                    )
+                    / (2 + time_step * (2 * alpha + wave_vectors ** 2))
+                )
+                next_wave_func_idx: np.ndarray = fft.ifft(
+                    next_wave_func_idx_fft
+                )
+                wave_func_idx_diff: float = np.abs(
+                    wave_func_idx - next_wave_func_idx
+                ).max()
+                wave_func_idx = next_wave_func_idx
+                logger.debug(
+                    {"BEPS step iter": idx, "Abs. diff": wave_func_idx_diff}
+                )
+                if wave_func_idx_diff <= time_step_abs_tol:
+                    break
+            else:
+                message = (
+                    "The BEPS Solver reached the maximum number of "
+                    "iterations during a single time step without "
+                    "converging to the prescribed tolerance."
+                )
+                logger.warning(
+                    {
+                        "message": message,
+                        "time_step_abs_tol": time_step_abs_tol,
+                        "wave_func_abs_diff": wave_func_idx_diff,
+                    }
+                )
+            # Get the norm of the next wave function approximation,
+            # and normalize it.
+            norm_wave_func_idx = np.sqrt(
+                np.sum(np.abs(wave_func_idx) ** 2) * step_size
+            )
+            next_wave_func_tdx = wave_func_idx / norm_wave_func_idx
+            # logger.debug({"wave_func": next_wave_func_tdx})
+            # Compare the difference between wave functions.
+            wave_func_tdx_diff: float = np.abs(
+                wave_func_tdx - next_wave_func_tdx
+            ).max()
+            # Reassign the initial wave function for the next time step.
+            wave_func_tdx = next_wave_func_tdx
+            logger.debug(
+                {"Time step": step_tdx + 1, "Abs. diff": wave_func_tdx_diff}
+            )
+            if wave_func_tdx_diff <= abs_tol:
+                break_evolve = True
+        else:
+            message = (
+                "The BEPS Solver reached the maximum number of "
+                "time steps without "
+                "converging to the prescribed tolerance."
+            )
+            logger.warning(
+                {
+                    "message": message,
+                    "abs_tol": abs_tol,
+                    "wave_func_abs_diff": wave_func_tdx_diff,
+                }
+            )
---
+++ tests/one_dim/test_gs_single.py
@@ -0,0 +1,108 @@
+import logging
+
+import numpy as np
+import pytest
+from hypothesis import given, settings
+from hypothesis import strategies as stg
+from matplotlib import pyplot
+from rich.logging import RichHandler
+
+from gspits import Mesh, TimeMesh
+from gspits.one_dim import HTHamiltonian
+from gspits.one_dim.gs_single import BEPSSolver, BEPSSolverState
+
+# Some parameter values that define a valid harmonic trap potential.
+FREQ = 1
+SCAT_LENGTH = 2e-4
+NUM_BOSONS = 10000
+
+logger = logging.getLogger("gspits.one_dim.gs_single")
+
+
+@given(freq=stg.floats(min_value=1, max_value=5))
+@settings(deadline=10000, max_examples=5)
+def test_beps_solver_ground_state(freq: float):
+    """Check that the solver runs."""
+    logger.addHandler(RichHandler())
+    hamiltonian = HTHamiltonian(
+        freq=freq, scat_length=SCAT_LENGTH, num_bosons=NUM_BOSONS
+    )
+    trap_size = hamiltonian.trap_size
+    logger.debug({"trap_size": trap_size})
+    mesh = Mesh(
+        lower_bound=-8 * trap_size, upper_bound=8 * trap_size, num_segments=256
+    )
+    ini_state = hamiltonian.gaussian_state(mesh)
+    time_mesh = TimeMesh(time_step=2 ** (-4), num_steps=128)
+    beps_solver = BEPSSolver(
+        hamiltonian=hamiltonian,
+        mesh=mesh,
+        time_mesh=time_mesh,
+        ini_state=ini_state,
+        abs_tol=1e-8,
+    )
+    ground_state = beps_solver.ground_state
+    assert ground_state.norm == pytest.approx(1, abs=1e-8)
+
+
+@pytest.mark.mpl_interactive
+def test_beps_solver_states():
+    """Check how the initial state evolves as the BEPSSolver runs."""
+    # logger.setLevel(logging.INFO)
+    logger.addHandler(RichHandler())
+    hamiltonian = HTHamiltonian(
+        freq=FREQ, scat_length=SCAT_LENGTH, num_bosons=NUM_BOSONS
+    )
+    trap_size = hamiltonian.trap_size
+    mesh = Mesh(
+        lower_bound=-4 * trap_size, upper_bound=4 * trap_size, num_segments=128
+    )
+    time_mesh = TimeMesh(time_step=2 ** (-4), num_steps=256)
+    ini_state = hamiltonian.gaussian_state(mesh)
+    beps_solver = BEPSSolver(
+        hamiltonian=hamiltonian,
+        mesh=mesh,
+        time_mesh=time_mesh,
+        ini_state=ini_state,
+        abs_tol=1e-8,
+    )
+    ground_state = ini_state
+    solver_state: BEPSSolverState
+    for idx, solver_state in enumerate(beps_solver):
+        logger.debug(
+            {
+                "energy": solver_state.energy,
+                "chemical_potential": solver_state.chemical_potential,
+            }
+        )
+        pyplot.plot(
+            solver_state.mesh.array, np.abs(solver_state.wave_func) ** 2
+        )
+        ground_state = solver_state.state
+
+    assert ground_state.norm == pytest.approx(1, abs=1e-8)
+    pyplot.show()
+
+
+@pytest.mark.mpl_interactive
+def test_view_beps_solver_ground_state():
+    """Shows how the BEPSSolver evolves a initial state to the ground state."""
+    # logger.setLevel(logging.INFO)
+    logger.addHandler(RichHandler())
+    hamiltonian = HTHamiltonian(
+        freq=FREQ, scat_length=SCAT_LENGTH, num_bosons=NUM_BOSONS
+    )
+    mesh = Mesh(lower_bound=-8, upper_bound=8, num_segments=256)
+    time_mesh = TimeMesh(time_step=2 ** (-4), num_steps=128)
+    ini_state = hamiltonian.gaussian_state(mesh)
+    beps_solver = BEPSSolver(
+        hamiltonian=hamiltonian,
+        mesh=mesh,
+        time_mesh=time_mesh,
+        ini_state=ini_state,
+        abs_tol=1e-5,
+    )
+    ground_state = beps_solver.ground_state
+    pyplot.plot(ground_state.mesh.array, np.abs(ground_state.wave_func) ** 2)
+    pyplot.plot(ini_state.mesh.array, np.abs(ini_state.wave_func) ** 2)
+    pyplot.show()
