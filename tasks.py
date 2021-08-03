a66177c37efeaea59c91179e52df2046c2fb355f
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2021-08-03 16:36:03 -0500

    Use `click` instead of `typer` and remove redundant development tasks

--- tasks.py
+++ tasks.py
@@ -10,9 +10,7 @@ from pathlib import Path
 from subprocess import run
 from typing import List
 
-import typer
-
-from gspits import __version__
+import click
 
 PROJECT_DIR = Path(__file__).parent
 SRC_DIR = PROJECT_DIR / "src"
@@ -20,8 +18,12 @@ TESTS_DIR = PROJECT_DIR / "tests"
 DOCS_DIR = PROJECT_DIR / "docs"
 DOCS_SOURCE_DIR = DOCS_DIR / "source"
 DOCS_BUILD_DIR = DOCS_DIR / "build"
+NOTEBOOKS_DIR = PROJECT_DIR / "notebooks"
 TASKS_FILE = PROJECT_DIR / "tasks.py"
 
+PYTHON_CMD = "python"
+POETRY_CMD = shutil.which("poetry")
+PIP_CMD = "pip"
 ISORT_CMD = "isort"
 BLACK_CMD = "black"
 PYDOCSTYLE_CMD = "pydocstyle"
@@ -33,123 +35,108 @@ SPHINX_BUILD_CMD = "sphinx-build"
 # Coverage report XML file.
 COVERAGE_XML = "coverage.xml"
 
-# Arguments to pass to subprocess.run for each task.
-BUILD_DOCS_ARGS = [
-    SPHINX_BUILD_CMD,
-    str(DOCS_SOURCE_DIR),
-    str(DOCS_BUILD_DIR),
-]
-FORMAT_ARGS = [
-    BLACK_CMD,
-    str(TASKS_FILE),
-    str(SRC_DIR),
-    str(TESTS_DIR),
-    str(DOCS_DIR),
-]
-ISORT_ARGS = [
-    ISORT_CMD,
-    str(TASKS_FILE),
-    str(SRC_DIR),
-    str(TESTS_DIR),
-]
-PYDOCSTYLE_ARGS = [
-    PYDOCSTYLE_CMD,
-    str(TASKS_FILE),
-    str(SRC_DIR),
-    str(TESTS_DIR),
-]
-FLAKE8_ARGS = [
-    FLAKE8_CMD,
-    str(TASKS_FILE),
-    str(SRC_DIR),
-    str(TESTS_DIR),
-    "--statistics",
-]
-PYTEST_ARGS = [
-    PYTEST_CMD,
-    "--cov",
-    "--cov-report",
-    "term-missing",
-    "--cov-report",
-    f"xml:./{COVERAGE_XML}",
-]
-MYPY_ARGS = [
-    MYPY_CMD,
-    str(TASKS_FILE),
-    str(SRC_DIR),
-]
-
 
 def _run(command: List[str]):
     """Run a subcommand through python subprocess.run routine."""
     # NOTE: See https://stackoverflow.com/a/32799942 in case we want to
     #  remove shell=True.
-    run(command)
+    return run(command)
 
 
-app = typer.Typer()
-
-
-@app.command()
-def black():
-    """Format the source code using black."""
-    _run(FORMAT_ARGS)
+app = click.Group("tasks")
 
 
 @app.command()
-def isort():
-    """Format imports using isort."""
-    _run(ISORT_ARGS)
+def version():
+    """Show the installed project version."""
+    import gspits
 
-
-@app.command()
-def pydocstyle():
-    """Run pydocstyle."""
-    _run(PYDOCSTYLE_ARGS)
-
-
-@app.command()
-def flake8():
-    """Run flake8 linter."""
-    _run(FLAKE8_ARGS)
-
-
-@app.command()
-def mypy():
-    """Run mypy."""
-    _run(MYPY_ARGS)
+    print(f"{gspits.metadata['name']} {gspits.__version__}")
 
 
 @app.command()
 def tests():
     """Run test suite."""
-    _run(PYTEST_ARGS)
-
-
-@app.command()
-def version():
-    """Project version."""
-    print(__version__)
+    pytest_args = [
+        PYTEST_CMD,
+        "--cov",
+        "--cov-report",
+        "term-missing",
+        "--cov-report",
+        f"xml:./{COVERAGE_XML}",
+    ]
+    _run(pytest_args)
 
 
 @app.command(name="format")
 def format_():
-    """Run all formatting tasks."""
-    _run(FORMAT_ARGS)
-    _run(ISORT_ARGS)
+    """Execute formatting tasks.
+
+    Format files using `black` together with `isort` to sort imports.
+    """
+    format_args = [
+        BLACK_CMD,
+        str(TASKS_FILE),
+        str(SRC_DIR),
+        str(TESTS_DIR),
+        str(DOCS_DIR),
+        str(NOTEBOOKS_DIR),
+    ]
+    isort_args = [
+        ISORT_CMD,
+        str(TASKS_FILE),
+        str(SRC_DIR),
+        str(TESTS_DIR),
+        str(DOCS_DIR),
+        str(NOTEBOOKS_DIR),
+    ]
+    _run(format_args)
+    _run(isort_args)
 
 
 @app.command()
 def typecheck():
-    """Run all typechecking tasks."""
-    _run(MYPY_ARGS)
+    """Execute typechecking tasks.
+
+    Execute `mypy` for static type checking.
+    """
+    mypy_args = [
+        MYPY_CMD,
+        str(TASKS_FILE),
+        str(SRC_DIR),
+        str(TESTS_DIR),
+        # str(DOCS_DIR),
+        # str(NOTEBOOKS_DIR),
+    ]
+    _run(mypy_args)
 
 
 @app.command()
 def lint():
-    """Run all linting tasks."""
-    _run(PYDOCSTYLE_ARGS)
-    _run(FLAKE8_ARGS)
+    """Execute linting tasks.
+
+    Check code style issues using `flake8` and `pydocstyle` to
+    check docstrings.
+    """
+    pydocstyle_args = [
+        PYDOCSTYLE_CMD,
+        str(TASKS_FILE),
+        str(SRC_DIR),
+        str(TESTS_DIR),
+        str(DOCS_DIR),
+        str(NOTEBOOKS_DIR),
+    ]
+    flake8_args = [
+        FLAKE8_CMD,
+        str(TASKS_FILE),
+        str(SRC_DIR),
+        str(TESTS_DIR),
+        str(DOCS_DIR),
+        str(NOTEBOOKS_DIR),
+        "--statistics",
+    ]
+    _run(pydocstyle_args)
+    _run(flake8_args)
 
 
 @unique
@@ -159,16 +146,30 @@ class DocFormat(str, Enum):
     HTML = "html"
 
 
-doc_format_spec = typer.Option(
-    default="html", help="Generated documentation format"
-)
+# Set HTML as the default document format.
+default_doc_format = DocFormat.HTML.name
+
+# List of allowed document formats.
+doc_formats = list(DocFormat.__members__.keys())
 
 
 @app.command()
-def build_docs(doc_format: DocFormat = doc_format_spec):
+@click.option(
+    "--doc-format",
+    type=click.Choice(doc_formats),
+    default=default_doc_format,
+    help=f"Generated documentation format. Defaults to {default_doc_format}.",
+)
+def build_docs(doc_format: str):
     """Build the documentation."""
-    BUILD_DOCS_ARGS.extend(["-b", doc_format])
-    _run(BUILD_DOCS_ARGS)
+    build_docs_args = [
+        SPHINX_BUILD_CMD,
+        str(DOCS_SOURCE_DIR),
+        str(DOCS_BUILD_DIR),
+    ]
+    doc_format_ = DocFormat[doc_format]
+    build_docs_args.extend(["-b", doc_format_])
+    _run(build_docs_args)
 
 
 @unique
@@ -178,13 +179,24 @@ class CleaningTask(str, Enum):
     DOCS = "docs"
 
 
-what_spec = typer.Argument(default=None, help="Cleaning task to perform")
+# Set DOCS as the default cleaning task.
+default_cleaning_task = CleaningTask.DOCS.name
+
+# List of allowed cleaning tasks.
+cleaning_tasks = list(CleaningTask.__members__.keys())
 
 
 @app.command()
-def clean(task: CleaningTask = what_spec):
-    """Clean any existing documentation."""
-    if task is None or task is CleaningTask.DOCS:
+@click.option(
+    "--task",
+    type=click.Choice(cleaning_tasks),
+    default=default_cleaning_task,
+    help=f"Cleaning task to perform. Defaults to {default_cleaning_task}.",
+)
+def clean(task: str):
+    """Clean project resources."""
+    task_ = None if task is None else CleaningTask[task]
+    if task_ is None or task_ is CleaningTask.DOCS:
         shutil.rmtree(DOCS_BUILD_DIR, ignore_errors=True)
 
 
