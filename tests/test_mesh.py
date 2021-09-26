bde1df9a9d2c908e1538ebd40f87630bac98b6ae
Omar Abel Rodríguez-López <oarodriguez.mx@gmail.com>
2021-09-26 01:36:47 -0500

    Remove aliases and update variable names

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -6,8 +6,7 @@ from attr import evolve
 from hypothesis import given
 from hypothesis import strategies as stg
 
-from gspits import Partition as Mesh
-from gspits import TimePartition as TimeMesh
+from gspits import Partition, TimePartition
 
 valid_lower_bound_stg = stg.floats(min_value=0, max_value=2)
 valid_upper_bound_stg = stg.floats(min_value=3, max_value=4)
@@ -15,17 +14,17 @@ valid_num_segments_steps_stg = stg.integers(min_value=1, max_value=512)
 
 
 @given(lower_bound=stg.floats(min_value=100, max_value=200))
-def test_mesh(lower_bound: float):
-    """Test spatial meshes with invalid bounds."""
+def test_partition(lower_bound: float):
+    """Test spatial partitions with invalid bounds."""
     with pytest.raises(ValueError):
-        Mesh(lower_bound, upper_bound=0, num_segments=128)
+        Partition(lower_bound, upper_bound=0, num_segments=128)
 
 
 @given(num_segments=stg.integers(min_value=-1000, max_value=0))
-def test_mesh_num_steps(num_segments: int):
-    """Test spatial meshes with invalid number of steps."""
+def test_partition_num_steps(num_segments: int):
+    """Test spatial partitions with invalid number of steps."""
     with pytest.raises(ValueError):
-        Mesh(lower_bound=0, upper_bound=1, num_segments=num_segments)
+        Partition(lower_bound=0, upper_bound=1, num_segments=num_segments)
 
 
 @given(
@@ -33,17 +32,17 @@ def test_mesh_num_steps(num_segments: int):
     upper_bound=valid_upper_bound_stg,
     num_segments=valid_num_segments_steps_stg,
 )
-def test_mesh_bounds(
+def test_partition_bounds(
     lower_bound: float, upper_bound: float, num_segments: int
 ):
-    """Test routines for creating spatial meshes."""
-    mesh = Mesh(lower_bound, upper_bound, num_segments)
-    mesh_as_array = mesh.array
-    assert mesh_as_array.min() == mesh.lower_bound
-    assert np.allclose(np.diff(mesh_as_array), mesh.step_size)
-    mesh = evolve(mesh, endpoint=True)
-    mesh_as_array = mesh.array
-    assert np.allclose(np.diff(mesh_as_array), mesh.step_size)
+    """Test routines for creating spatial partitions."""
+    partition = Partition(lower_bound, upper_bound, num_segments)
+    partition_array = partition.array
+    assert partition_array.min() == partition.lower_bound
+    assert np.allclose(np.diff(partition_array), partition.step_size)
+    partition = evolve(partition, endpoint=True)
+    partition_array = partition.array
+    assert np.allclose(np.diff(partition_array), partition.step_size)
 
 
 # See https://stackoverflow.com/questions/19141432 for details.
@@ -54,19 +53,19 @@ valid_ini_time_stg = stg.floats(min_value=-128, max_value=128)
 @given(
     num_steps=stg.integers(min_value=-1000, max_value=0),
 )
-def test_time_mesh_num_steps(num_steps: int):
-    """Testing temporal meshes with an invalid number of steps."""
+def test_time_partition_num_steps(num_steps: int):
+    """Check temporal partitions with an invalid number of steps."""
     with pytest.raises(ValueError):
-        TimeMesh(time_step=1e-2, num_steps=num_steps)
+        TimePartition(time_step=1e-2, num_steps=num_steps)
 
 
 @given(
     time_step=stg.floats(min_value=-1e2, max_value=0),
 )
-def test_time_mesh_time_step(time_step: float):
-    """Testing temporal meshes with an invalid time step."""
+def test_time_partition_time_step(time_step: float):
+    """Check temporal partitions with an invalid time step."""
     with pytest.raises(ValueError):
-        TimeMesh(time_step=time_step, num_steps=128)
+        TimePartition(time_step=time_step, num_steps=128)
 
 
 @given(
@@ -74,14 +73,14 @@ def test_time_mesh_time_step(time_step: float):
     num_steps=valid_num_segments_steps_stg,
     ini_time=valid_ini_time_stg,
 )
-def test_times_mesh(time_step: float, num_steps: int, ini_time: float):
-    """Testing routines for creating temporal meshes."""
-    time_mesh = TimeMesh(
+def test_time_partition(time_step: float, num_steps: int, ini_time: float):
+    """Check routines for creating temporal partitions."""
+    time_mesh = TimePartition(
         time_step=time_step, num_steps=num_steps, ini_time=ini_time
     )
-    mesh_as_array = time_mesh.array
-    assert mesh_as_array.min() == time_mesh.ini_time
-    assert np.allclose(np.diff(mesh_as_array), time_mesh.time_step)
+    partition_array = time_mesh.array
+    assert partition_array.min() == time_mesh.ini_time
+    assert np.allclose(np.diff(partition_array), time_mesh.time_step)
     time_mesh = evolve(time_mesh, endpoint=True)
-    mesh_as_array = time_mesh.array
-    assert np.allclose(np.diff(mesh_as_array), time_mesh.time_step)
+    partition_array = time_mesh.array
+    assert np.allclose(np.diff(partition_array), time_mesh.time_step)
